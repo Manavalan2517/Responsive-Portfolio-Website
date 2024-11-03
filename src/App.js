1b98459cb2e7ce21dc96d77d69a44e6d8d15701b
Mano <112639423+Manavalan2517@users.noreply.github.com>
2024-11-03 21:31:21 +0530

    added analytics feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { Pages } from './components/pages/Pages';
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div>
       <Pages />
+      <Analytics />
     </div>
   );
 }
